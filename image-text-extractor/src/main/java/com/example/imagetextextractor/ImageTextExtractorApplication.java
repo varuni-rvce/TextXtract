5d52394c3fb0a5cf06ebb352bf2a4c8171371e1f
unknown <1jt19is054@jyothy.it.ac.in>
2024-09-12 11:19:03 +0530

    Added image-text-extractor folder

---
+++ image-text-extractor/src/main/java/com/example/imagetextextractor/ImageTextExtractorApplication.java
@@ -0,0 +1,12 @@
+package com.example.imagetextextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageTextExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageTextExtractorApplication.class, args);
+    }
+}
